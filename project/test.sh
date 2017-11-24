7fcda799728c6cece1654ba80dd098e38746cd30
Fabio Carvalho <fabio.coelho@resultadosdigitais.com.br>
2017-11-24 13:50:08 -0200

    Update endpoints ip in test

--- project/test.sh
+++ project/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for j in {1..100}
+for j in {1..1000}
 do
   for i in {1..3}
   do
-    curl -d "{\"email\":\"email$i@test.com\"}" -H "Content-Type: application/json" -X POST http://192.168.99.100:8081
-    curl -d "{\"email\":\"email$i@test.com\"}" -H "Content-Type: application/json" -X POST http://192.168.99.100:8082
-    curl -d "{\"email\":\"email$i@test.com\"}" -H "Content-Type: application/json" -X POST http://192.168.99.100:8083
+    curl -d "{\"email\":\"email$i@test.com\"}" -H "Content-Type: application/json" -X POST http://0.0.0.0:8081
+    curl -d "{\"email\":\"email$i@test.com\"}" -H "Content-Type: application/json" -X POST http://0.0.0.0:8082
+    curl -d "{\"email\":\"email$i@test.com\"}" -H "Content-Type: application/json" -X POST http://0.0.0.0:8083
   done
 done
